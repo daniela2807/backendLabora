bfa3c5c91eda3ab0ae5561c9b087fe5aeabdb298
Daniela Gutierrez Plascencia <al236595@edu.uaa.mx>
2021-10-19 11:55:17 -0500

    first

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '29ade850d2416e9a5f778f33848427f3'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '5790b44d-85e0-4094-aacd-7f76eba5dc73'
+};
